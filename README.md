c2c0fc942c2752a3be47359b40bb5f76973fed32
simonwu-dev <simonwu@gmail.com>
2020-04-30 16:28:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-4fcrsf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-4fcrsf)
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyD2G75QvVceuLVTyQI-0ERAHNVnX6ehzck",
+    authDomain: "friendlymarket-2c043.firebaseapp.com",
+    databaseURL: "https://friendlymarket-2c043.firebaseio.com",
+    projectId: "friendlymarket-2c043",
+    storageBucket: "friendlymarket-2c043.appspot.com",
+    messagingSenderId: "434457387852",
+    appId: "1:434457387852:web:e6d27e547ca0474f4a25e9"
+};
+
+export default firebaseConfig;
